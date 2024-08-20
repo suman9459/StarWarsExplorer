fcbf9ffdf621b2004bace633feeadc5e407c16e2
suman9459 <saisuman9459@gmail.com>
2024-08-20 10:47:30 -0400

    Initial commit

---
+++ StarWarsExplorer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StarWarsExplorer
+//
+//  Created by Sai Suman Pothedar on 8/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

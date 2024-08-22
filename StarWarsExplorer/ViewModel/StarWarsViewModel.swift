d9a009f9fd64b97896f679356460cee54bd3da7c
suman9459 <saisuman9459@gmail.com>
2024-08-22 13:27:26 -0400

    secondary commit

--- StarWarsExplorer/ViewModel/StarWarsViewModel.swift
+++ StarWarsExplorer/ViewModel/StarWarsViewModel.swift
@@ -10,24 +10,27 @@ import Combine
 
 class StarWarsViewModel: ObservableObject {
     @Published var characters = [StarWarsCharacter]()
+    @Published var isLoading = false
     private var cancellable: AnyCancellable?
 
     func fetchCharacters() {
+        isLoading = true
         let url = URL(string: "https://swapi.dev/api/people/")!
         
         cancellable = URLSession.shared.dataTaskPublisher(for: url)
             .map { $0.data }
             .decode(type: StarWarsResponse.self, decoder: JSONDecoder())
             .receive(on: DispatchQueue.main)
-            .sink(receiveCompletion: { completion in
+            .sink(receiveCompletion: { [weak self] completion in
+                self?.isLoading = false
                 switch completion {
                 case .finished:
                     break
                 case .failure(let error):
                     print("Error fetching characters: \(error)")
                 }
-            }, receiveValue: { response in
-                self.characters = response.results
+            }, receiveValue: { [weak self] response in
+                self?.characters = response.results
             })
     }
 }
--- StarWarsExplorer/ViewModel/StarshipViewModel.swift
+++ StarWarsExplorer/ViewModel/StarshipViewModel.swift
@@ -10,24 +10,27 @@ import Combine
 
 class StarshipViewModel: ObservableObject {
     @Published var starships = [Starship]()
+    @Published var isLoading = false
     private var cancellable: AnyCancellable?
 
     func fetchStarships() {
+        isLoading = true
         let url = URL(string: "https://swapi.dev/api/starships/")!
         
         cancellable = URLSession.shared.dataTaskPublisher(for: url)
             .map { $0.data }
             .decode(type: StarshipResponse.self, decoder: JSONDecoder())
             .receive(on: DispatchQueue.main)
-            .sink(receiveCompletion: { completion in
+            .sink(receiveCompletion: { [weak self] completion in
+                self?.isLoading = false
                 switch completion {
                 case .finished:
                     break
                 case .failure(let error):
                     print("Error fetching starships: \(error)")
                 }
-            }, receiveValue: { response in
-                self.starships = response.results
+            }, receiveValue: { [weak self] response in
+                self?.starships = response.results
             })
     }
 }
